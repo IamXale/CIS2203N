dcc2543c44bce4fe02c2c099c020deb7531b88ba
Jeric Tan <thejerictan@gmail.com>
2026-03-26 17:01:19 +0800

    feat: add state preservation and custom operator.

--- app/src/main/java/com/example/mockmidterm/MainActivity.java
+++ app/src/main/java/com/example/mockmidterm/MainActivity.java
@@ -8,6 +8,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String KEY_CURRENT_INPUT = "current_input";
+    private static final String KEY_OPERAND_A = "operand_a";
+    private static final String KEY_OPERAND_B = "operand_b";
+    private static final String KEY_OPERATOR = "operator";
+    private static final String KEY_IS_OPERATOR_SELECTED = "is_operator_selected";
+    private static final String KEY_IS_RESULT_DISPLAYED = "is_result_displayed";
+    private static final String KEY_IS_ERROR_STATE = "is_error_state";
+    private static final String KEY_CALCULATION_TEXT = "calculation_text";
+    private static final String KEY_RESULT_TEXT = "result_text";
+
     private TextView calculationTextView;
     private TextView resultTextView;
     private String currentInput = "";
@@ -19,6 +29,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private boolean isError = false;
 
     @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putString(KEY_CURRENT_INPUT, currentInput);
+        outState.putString(KEY_OPERAND_A, operandA);
+        outState.putString(KEY_OPERAND_B, operandB);
+        outState.putString(KEY_OPERATOR, operator);
+        outState.putBoolean(KEY_IS_OPERATOR_SELECTED, isOperatorSelected);
+        outState.putBoolean(KEY_IS_RESULT_DISPLAYED, isResultDisplayed);
+        outState.putBoolean(KEY_IS_ERROR_STATE, isError);
+
+        outState.putString(KEY_CALCULATION_TEXT, calculationTextView.getText().toString());
+        outState.putString(KEY_RESULT_TEXT, resultTextView.getText().toString());
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -26,16 +52,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         calculationTextView = findViewById(R.id.calculation);
         resultTextView = findViewById(R.id.result);
 
+        if(savedInstanceState != null) {
+            restoreState(savedInstanceState);
+        }
+
         initializeButtons();
     }
 
+    private void restoreState(Bundle savedInstanceState) {
+        currentInput = savedInstanceState.getString(KEY_CURRENT_INPUT, "");
+        operandA = savedInstanceState.getString(KEY_OPERAND_A, "");
+        operandB = savedInstanceState.getString(KEY_OPERAND_B, "");
+        operator = savedInstanceState.getString(KEY_OPERATOR, "");
+        isOperatorSelected = savedInstanceState.getBoolean(KEY_IS_OPERATOR_SELECTED, false);
+        isResultDisplayed = savedInstanceState.getBoolean(KEY_IS_RESULT_DISPLAYED, false);
+        isError = savedInstanceState.getBoolean(KEY_IS_ERROR_STATE, false);
+
+        String calculationText = savedInstanceState.getString(KEY_CALCULATION_TEXT, "");
+        String resultText = savedInstanceState.getString(KEY_RESULT_TEXT, "");
+
+        calculationTextView.setText(calculationText);
+        resultTextView.setText(resultText);
+    }
     private void initializeButtons() {
         //Numbers
         int[] allButtons = {
                 R.id.btn0, R.id.btn1, R.id.btn2, R.id.btn3, R.id.btn4,
                 R.id.btn5, R.id.btn6, R.id.btn7, R.id.btn8, R.id.btn9,
                 R.id.btnAdd, R.id.btnSubtract, R.id.btnMultiply, R.id.btnDivide,
-                R.id.btnPeriod, R.id.btnEquals, R.id.btnAC, R.id.btnDelete
+                R.id.btnPeriod, R.id.btnEquals, R.id.btnAC, R.id.btnDelete,
+                R.id.btnCustom
         };
 
         for (int id : allButtons) {
@@ -58,6 +104,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         else if (id == R.id.btnPeriod) {
             handleDecimal();
+        }
+        else if (id == R.id.btnCustom) {
+            customOperator();
         }
         else {
             String text = ((Button) v).getText().toString();
@@ -143,7 +192,51 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             showError("Calculation error");
         }
     }
+    private void customOperator() {
+        if (isError) {
+            return;
+        }
 
+        String currentValue;
+
+        if (isResultDisplayed && !currentInput.isEmpty()) {
+            currentValue = currentInput;
+        } else if (!currentInput.isEmpty()) {
+            currentValue = currentInput;
+        } else if (!operandA.isEmpty()) {
+            currentValue = operandA;
+        } else {
+            showError("Nothing to multiply");
+            return;
+        }
+
+        try {
+            double value = Double.parseDouble(currentValue);
+
+            double result = value * 918;
+
+            DecimalFormat df = new DecimalFormat("0.##########");
+            String formattedResult = df.format(result);
+
+            String equation = currentValue + " × 918 =";
+            calculationTextView.setText(equation);
+
+            resultTextView.setText(formattedResult);
+
+            currentInput = formattedResult;
+            operandA = formattedResult;
+            operandB = "";
+            operator = "";
+            isOperatorSelected = false;
+            isResultDisplayed = true;
+            isError = false;
+
+        } catch (NumberFormatException e) {
+            showError("Invalid number format");
+        } catch (Exception e) {
+            showError("Calculation error");
+        }
+    }
     private void handleDecimal() {
         if (isError) {
             return;
